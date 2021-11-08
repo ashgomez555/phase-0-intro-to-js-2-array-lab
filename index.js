cfde5420e7c02ab84f088193fa0e3190c1538c74
Ashley Gomez <ash.gomez555@gmail.com>
2021-11-08 12:16:24 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() { 
+    cats.shift()
+}
+
+
+function appendCat(name) {
+    const coolCats = [...cats]
+    coolCats.push(name)
+    return coolCats
+}
+
+function prependCat(name) {
+    const coolCats = [...cats]
+    coolCats.unshift(name)
+    return coolCats
+}
+
+function removeLastCat() {
+    const coolCats = [...cats]
+    coolCats.pop()
+    return coolCats
+}
+
+function removeFirstCat() {
+    const coolCats = [...cats]
+    coolCats.shift()
+    return coolCats
+}
